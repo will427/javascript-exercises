c2e735e5474112f0b5174c67de28e332aabba776
will427 <willian.wwc24@gmail.com>
2025-01-10 09:38:16 -0600

    changes

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,19 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+
+    if(num < 0)
+    {
+        return "ERROR";
+    }else
+    {
+        if(num === undefined)
+            {
+                num = Math.floor(Math.random * 1000);
+                return str.repeat(num);
+            }else
+            {
+                return str.repeat(num);
+            }
+    }
 
 };
 
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    // str.split("").reverse().join("")
+    let arr = [];
+    for(let i = 0; i < str.length; i++)
+        {
+            arr.push(str.charAt(str.length - (i+1)));
+        }
+    return arr.join("");  
 };
 
 // Do not edit below this line
