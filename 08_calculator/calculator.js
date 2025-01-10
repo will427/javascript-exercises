d28115147b9520f361d6da13d5a9685b18f28f35
will427 <willian.wwc24@gmail.com>
2025-01-10 09:40:08 -0600

    modified

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,70 @@
-const add = function() {
-	
+const add = function(one, two) {
+  return one + two;
 };
 
-const subtract = function() {
-	
+const subtract = function (one, two) {
+	return one - two;
 };
 
-const sum = function() {
-	
+const sum = function(arrayOfNumbers) {
+
+  if(arrayOfNumbers.length === 0)
+    {
+      return 0;
+    }else if(arrayOfNumbers.length === 1)
+      {
+        return arrayOfNumbers[0];
+      }
+    else
+    {
+      let counter = 0;
+      let sum = 0;
+      while(counter < arrayOfNumbers.length)
+        {
+          sum += arrayOfNumbers[counter];
+          counter++;
+        }
+        return sum;
+    }
+
+
 };
 
-const multiply = function() {
+const multiply = function(arrayOfNumbers) {
 
+  let counter = 0;
+  let result = 1;
+      
+      for (let  i= 0;  i< arrayOfNumbers.length; i++) 
+      {
+        let temporalMultiplication = result * arrayOfNumbers[i];
+        result = temporalMultiplication;
+      }
+      return result;
 };
 
-const power = function() {
-	
+const power = function(one, two) {
+	return Math.pow(one,two);
 };
 
-const factorial = function() {
+const factorial = function(number)
+{
 	
+  if(number === 0)
+    {
+      return 1;
+    }
+    else
+    {
+      let result = 1;
+      for(let i = number; 1 <= i; i--)
+        {
+          let holder = result * i;
+          result = holder;
+        }
+        return result;
+    }
+
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let lowerCaseString = str.toLowerCase();
+    let noWhiteSpacesString = lowerCaseString.replaceAll(" ", "");
+    let handlePunctuation = noWhiteSpacesString.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g,"");
+    let backwardsString = handlePunctuation.split("").reverse().join("");
 
+    if(handlePunctuation === backwardsString)
+    {
+        return true;
+    }else
+    {
+        return false;
+    }
 };
 
 // Do not edit below this line
